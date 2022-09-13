3cd0854ee91537c56b69ced2c8f42df65abcccc6
Tomasz Abram <tomasz.abram7@gmail.com>
2022-09-13 18:56:38 +0200

    Clear board service

---
+++ src/main/java/com/tabram/sudokusolver/service/ClearBoardService.java
@@ -0,0 +1,27 @@
+package com.tabram.sudokusolver.service;
+
+import com.tabram.sudokusolver.model.SudokuBoard;
+import com.tabram.sudokusolver.repository.SudokuBoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClearBoardService {
+
+    SudokuBoardRepository sudokuBoardRepository;
+
+    @Autowired
+    public ClearBoardService(SudokuBoardRepository sudokuBoardRepository) {
+        this.sudokuBoardRepository = sudokuBoardRepository;
+    }
+
+    public void clearBoard() {
+        SudokuBoard sudokuBoard = sudokuBoardRepository.getSudokuBoard();
+        for (int row = 0; row < sudokuBoardRepository.getSudokuBoard().getSudokuSize(); row++) {
+            for (int column = 0; column < sudokuBoardRepository.getSudokuBoard().getSudokuSize(); column++) {
+                sudokuBoard.getBoard()[row][column] = 0;
+            }
+        }
+        sudokuBoardRepository.setSudokuBoard(sudokuBoard);
+    }
+}
