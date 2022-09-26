31317455cd8b20ff7e8c6e3f60619383c2633fa3
Tomasz Abram <tomasz.abram7@gmail.com>
2022-09-26 23:47:36 +0200

    Thymeleaf -> Controller upload file

--- src/main/java/com/tabram/sudokusolver/controller/InputOutputController.java
+++ src/main/java/com/tabram/sudokusolver/controller/InputOutputController.java
@@ -7,24 +7,27 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping({"/", "/home"})
 public class InputOutputController {
 
+    private static final String HOME = "redirect:/";
+
     private final JsonExporterService jsonExporterService;
 
     private final SudokuBoardRepository sudokuBoardRepository;
 
-    public InputOutputController(JsonExporterService jsonExporterService, SudokuBoardRepository sudokuBoardRepository) {
 
+    public InputOutputController(JsonExporterService jsonExporterService, SudokuBoardRepository sudokuBoardRepository) {
         this.jsonExporterService = jsonExporterService;
         this.sudokuBoardRepository = sudokuBoardRepository;
     }
 
-    @GetMapping("/save-file")
+    @GetMapping("/download-file")
     public ResponseEntity<byte[]> saveFile() {
 
         SudokuBoardObject sudokuBoardObject = sudokuBoardRepository.getSudokuBoardObject();
@@ -40,6 +43,13 @@ public class InputOutputController {
                 .contentLength(sudokuJsonBytes.length)
                 .body(sudokuJsonBytes);
     }
+
+    @PostMapping("/upload-file")
+    public String uploadFile(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("message","You successfully uploaded " + file.getOriginalFilename() + "!");
+        return HOME;
+    }
+
 }
 
 
