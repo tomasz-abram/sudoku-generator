98412cd46526b9a39766e800477857a09abb8322
Tomasz Abram <tomasz.abram7@gmail.com>
2022-10-04 15:58:53 +0200

    Copy sudokuObject to temp when game is generated

--- src/main/java/com/tabram/sudokusolver/controller/GenerateGameController.java
+++ src/main/java/com/tabram/sudokusolver/controller/GenerateGameController.java
@@ -2,10 +2,8 @@ package com.tabram.sudokusolver.controller;
 
 import com.tabram.sudokusolver.model.SudokuObject;
 import com.tabram.sudokusolver.repository.SudokuObjectRepository;
-import com.tabram.sudokusolver.service.BoardValueManipulation;
-import com.tabram.sudokusolver.service.ClearBoardService;
-import com.tabram.sudokusolver.service.GenerateSudokuGameService;
-import com.tabram.sudokusolver.service.SudokuSolveService;
+import com.tabram.sudokusolver.repository.TempSudokuObject;
+import com.tabram.sudokusolver.service.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,16 +16,19 @@ public class GenerateGameController {
     private final SudokuObjectRepository sudokuObjectRepository;
     private final BoardValueManipulation boardValueManipulation;
     private final SudokuSolveService sudokuSolveService;
+    private final CopyObjectService copyObjectService;
+    private final TempSudokuObject tempSudokuObject;
 
-
-    public GenerateGameController(GenerateSudokuGameService generateSudokuGameService, ClearBoardService clearBoardService, SudokuObjectRepository sudokuObjectRepository, BoardValueManipulation boardValueManipulation, SudokuSolveService sudokuSolveService) {
+    public GenerateGameController(GenerateSudokuGameService generateSudokuGameService, ClearBoardService clearBoardService, SudokuObjectRepository sudokuObjectRepository, BoardValueManipulation boardValueManipulation, SudokuSolveService sudokuSolveService, CopyObjectService copyObjectService, TempSudokuObject tempSudokuObject) {
         this.generateSudokuGameService = generateSudokuGameService;
         this.clearBoardService = clearBoardService;
         this.sudokuObjectRepository = sudokuObjectRepository;
         this.boardValueManipulation = boardValueManipulation;
         this.sudokuSolveService = sudokuSolveService;
-
+        this.copyObjectService = copyObjectService;
+        this.tempSudokuObject = tempSudokuObject;
     }
+
 
     @GetMapping("/generate-easy-game")
     public String generateGame(@RequestParam String level) {
@@ -50,6 +51,7 @@ public class GenerateGameController {
         boardValueManipulation.changeNullToZeroOnBoard(sudokuObject);
         generateSudokuGameService.generate(sudokuObject);
         sudokuSolveService.solveBoard(sudokuObject);
+        tempSudokuObject.setSudokuObject(copyObjectService.deepCopy(sudokuObject));
         generateSudokuGameService.randomCleanBoard(sudokuObject, percent);
         return HOME;
     }
