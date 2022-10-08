4a611fc0893445ec14b00750619b0c6ce1bff043
Tomasz Abram <tomasz.abram7@gmail.com>
2022-10-08 14:16:49 +0200

    Test MapperService

---
+++ src/test/java/com/tabram/sudokusolver/service/MapperServiceTest.java
@@ -0,0 +1,51 @@
+package com.tabram.sudokusolver.service;
+
+import com.tabram.sudokusolver.dto.SudokuObjectDto;
+import com.tabram.sudokusolver.model.SudokuObject;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class MapperServiceTest {
+
+    private MapperService underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new MapperService();
+    }
+
+    @Test
+    void mapperToSudokuBoardObject() {
+        Integer[][] board = {
+                {0, 0, 4, 0, 6, 0, 0, 0, 2},
+                {3, 0, 0, 5, 0, 0, 0, 0, 7},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 8, 0, 0, 0, 0},
+                {0, 3, 0, 0, 4, 0, 7, 0, 8},
+                {5, 0, 0, 0, 7, 0, 0, 0, 6},
+                {0, 0, 0, 0, 0, 0, 1, 8, 0},
+                {2, 0, 0, 9, 0, 0, 0, 0, 3},
+                {0, 1, 0, 6, 0, 0, 0, 2, 0}
+        };
+        SudokuObjectDto testSudokuObjectDto = new SudokuObjectDto();
+        testSudokuObjectDto.setBoard(board);
+        testSudokuObjectDto.setSudokuSize(9);
+        testSudokuObjectDto.setQuantityBoxesHeight(3);
+        testSudokuObjectDto.setQuantityBoxesWidth(3);
+
+        SudokuObject actualSudoku = underTest.mapperToSudokuBoardObject(testSudokuObjectDto);
+
+        assertAll(
+                () -> assertEquals(testSudokuObjectDto.getSudokuSize(), actualSudoku.getSudokuSize()),
+                () -> assertEquals(testSudokuObjectDto.getQuantityBoxesHeight(), actualSudoku.getQuantityBoxesHeight()),
+                () -> assertEquals(testSudokuObjectDto.getQuantityBoxesWidth(), actualSudoku.getQuantityBoxesWidth()),
+                () -> assertThat(testSudokuObjectDto.getBoard()).isDeepEqualTo(actualSudoku.getBoard()));
+    }
+}
