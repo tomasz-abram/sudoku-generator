eb09ee56aa23b8196626940ca57843002805897d
Tomasz Abram <tomasz.abram7@gmail.com>
2022-10-08 14:17:59 +0200

    Test MapperService

--- src/test/java/com/tabram/sudokusolver/service/MapperServiceTest.java
+++ src/test/java/com/tabram/sudokusolver/service/MapperServiceTest.java
@@ -3,6 +3,7 @@ package com.tabram.sudokusolver.service;
 import com.tabram.sudokusolver.dto.SudokuObjectDto;
 import com.tabram.sudokusolver.model.SudokuObject;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -21,31 +22,34 @@ class MapperServiceTest {
         underTest = new MapperService();
     }
 
-    @Test
-    void mapperToSudokuBoardObject() {
-        Integer[][] board = {
-                {0, 0, 4, 0, 6, 0, 0, 0, 2},
-                {3, 0, 0, 5, 0, 0, 0, 0, 7},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {1, 0, 0, 0, 8, 0, 0, 0, 0},
-                {0, 3, 0, 0, 4, 0, 7, 0, 8},
-                {5, 0, 0, 0, 7, 0, 0, 0, 6},
-                {0, 0, 0, 0, 0, 0, 1, 8, 0},
-                {2, 0, 0, 9, 0, 0, 0, 0, 3},
-                {0, 1, 0, 6, 0, 0, 0, 2, 0}
-        };
-        SudokuObjectDto testSudokuObjectDto = new SudokuObjectDto();
-        testSudokuObjectDto.setBoard(board);
-        testSudokuObjectDto.setSudokuSize(9);
-        testSudokuObjectDto.setQuantityBoxesHeight(3);
-        testSudokuObjectDto.setQuantityBoxesWidth(3);
+    @Nested
+    class MapperTOSudokuBoardObject {
+        @Test
+        void mapperToSudokuBoardObject() {
+            Integer[][] board = {
+                    {0, 0, 4, 0, 6, 0, 0, 0, 2},
+                    {3, 0, 0, 5, 0, 0, 0, 0, 7},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {1, 0, 0, 0, 8, 0, 0, 0, 0},
+                    {0, 3, 0, 0, 4, 0, 7, 0, 8},
+                    {5, 0, 0, 0, 7, 0, 0, 0, 6},
+                    {0, 0, 0, 0, 0, 0, 1, 8, 0},
+                    {2, 0, 0, 9, 0, 0, 0, 0, 3},
+                    {0, 1, 0, 6, 0, 0, 0, 2, 0}
+            };
+            SudokuObjectDto testSudokuObjectDto = new SudokuObjectDto();
+            testSudokuObjectDto.setBoard(board);
+            testSudokuObjectDto.setSudokuSize(9);
+            testSudokuObjectDto.setQuantityBoxesHeight(3);
+            testSudokuObjectDto.setQuantityBoxesWidth(3);
 
-        SudokuObject actualSudoku = underTest.mapperToSudokuBoardObject(testSudokuObjectDto);
+            SudokuObject actualSudoku = underTest.mapperToSudokuBoardObject(testSudokuObjectDto);
 
-        assertAll(
-                () -> assertEquals(testSudokuObjectDto.getSudokuSize(), actualSudoku.getSudokuSize()),
-                () -> assertEquals(testSudokuObjectDto.getQuantityBoxesHeight(), actualSudoku.getQuantityBoxesHeight()),
-                () -> assertEquals(testSudokuObjectDto.getQuantityBoxesWidth(), actualSudoku.getQuantityBoxesWidth()),
-                () -> assertThat(testSudokuObjectDto.getBoard()).isDeepEqualTo(actualSudoku.getBoard()));
+            assertAll(
+                    () -> assertEquals(testSudokuObjectDto.getSudokuSize(), actualSudoku.getSudokuSize()),
+                    () -> assertEquals(testSudokuObjectDto.getQuantityBoxesHeight(), actualSudoku.getQuantityBoxesHeight()),
+                    () -> assertEquals(testSudokuObjectDto.getQuantityBoxesWidth(), actualSudoku.getQuantityBoxesWidth()),
+                    () -> assertThat(testSudokuObjectDto.getBoard()).isDeepEqualTo(actualSudoku.getBoard()));
+        }
     }
 }
